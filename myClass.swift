5a03ed49de595dc39cf61834b6b8443e809948a2
Serrano Molina <psmolina@indra.es>
2024-11-19 12:36:52 +0100

    Second commit

---
+++ myClass.swift
@@ -0,0 +1,14 @@
+class MyClass {
+    var name: String
+    var age: Int
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    func printName() {
+        print("Name: \(name)")
+    }
+    func printAge() {
+        print("Age: \(age)")
+    }
+}
